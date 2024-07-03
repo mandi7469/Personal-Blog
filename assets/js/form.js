02c68de6b8785a6685e9db1b3618b24164237bce
mandi7469 <mandi7469@aol.com>
2024-07-03 17:18:18 -0400

    created function to redirect the page when submitting the blog

--- assets/js/form.js
+++ assets/js/form.js
@@ -3,37 +3,38 @@ const titleInput = document.querySelector("#title");
 const contentInput = document.querySelector("#content");
 const submitInput = document.querySelector("#submit");
 
-let entries = []
+let entries = [];
 
 function init() {
-    const storedEntries =JSON.parse(localStorage.getItem("blogForm"));
+  const storedEntries = JSON.parse(localStorage.getItem("blogForm"));
 
-    if (storedEntries !== null) {
-        entries = entries.concat(storedEntries)
-    }
-
-    //renderEntries();
+  if (storedEntries !== null) {
+    entries = entries.concat(storedEntries);
+  }
 }
 
+//redirects to blog page
+function renderBlog() {
+  window.location.href = "blog.html";
+}
 
-function showResponse(event) {
-    event.preventDefault();
-    console.log(usernameInput.value, titleInput.value, contentInput.value);
-    
-    const blogForm = {
-        username: username.value.trim(),
-        title: title.value.trim(),
-        content: content.value.trim(),
-    };
+function handleInput(event) {
+  event.preventDefault();
+  console.log(usernameInput.value, titleInput.value, contentInput.value);
 
-    entries.push(blogForm);
+  const blogForm = {
+    username: username.value.trim(),
+    title: title.value.trim(),
+    content: content.value.trim(),
+  };
 
+  entries.push(blogForm);
 
-    localStorage.setItem("blogForm", JSON.stringify(entries));
+  localStorage.setItem("blogForm", JSON.stringify(entries));
 
-    let test =''
+  renderBlog();
 }
 // Add listener to submit element
-submitInput.addEventListener('click', showResponse);
+submitInput.addEventListener("click", handleInput);
 
 init();
