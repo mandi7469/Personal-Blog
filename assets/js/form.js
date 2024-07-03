666588fee20ce9c0de5343965479cba55680a51e
mandi7469 <mandi7469@aol.com>
2024-07-02 20:32:56 -0400

    started building the js file to hold blog post data to local storage

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,1 +1,39 @@
-console.log('This is the form js');
+const usernameInput = document.querySelector("#username");
+const titleInput = document.querySelector("#title");
+const contentInput = document.querySelector("#content");
+const submitInput = document.querySelector("#submit");
+
+const entries = []
+
+function init() {
+    const storedEntries =JSON.parse(localStorage.getItem("blogForm"));
+
+    if (storedEntries !== null) {
+        entries = storedEntries;
+    }
+    //renderEntries();
+}
+
+
+function showResponse(event) {
+    event.preventDefault();
+    console.log(usernameInput.value, titleInput.value, contentInput.value);
+    
+    const blogForm = {
+        username: username.value.trim(),
+        title: title.value.trim(),
+        content: content.value.trim(),
+    };
+
+    entries.push(blogForm);
+    
+
+
+    localStorage.setItem("blogForm", JSON.stringify(blogForm));
+
+    let test =''
+}
+// Add listener to submit element
+submitInput.addEventListener('click', showResponse);
+
+init();
