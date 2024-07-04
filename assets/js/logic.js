70c51af1fe0b308a65efc113c78ea99d86fb797a
mandi7469 <mandi7469@aol.com>
2024-07-03 22:14:56 -0400

    added light/dark toggle and styling

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,1 +1,18 @@
-console.log('This is the logic js');
+const themeSwitcher = document.querySelector("#theme-switcher");
+const container = document.querySelector(".container");
+
+let mode = "dark"
+
+themeSwitcher.addEventListener("click", function (){
+    
+  if (mode === "dark") {
+    mode = "light";
+    container.setAttribute("class", "light");
+  }
+
+  else {
+    mode = "dark";
+    container.setAttribute("class", "dark");
+  }
+  console.log();
+})
